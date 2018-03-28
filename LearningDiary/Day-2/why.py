3792414ac1f3b1dabbd49f562d836cccc4efbaa5
Yi-Hua Liang <yihua1218@gmail.com>
2018-03-28 20:54:31 +0800

    Why we write function.

---
+++ LearningDiary/Day-2/why.py
@@ -0,0 +1,12 @@
+
+def salestock(name):
+  print("連上網路")
+  print("下載", name, "股票資訊")
+  print("賣出", name, "股票")
+  print("轉帳")
+  print("斷網")
+
+salestock("A")
+salestock("B")
+salestock("C")
+salestock("D")
