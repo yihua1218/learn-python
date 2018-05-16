4a37178d3ad364a5e454425673a26ea3c5d74d78
Yi-Hua Liang <yihua1218@gmail.com>
2018-05-16 19:56:56 +0800

    Add import example.

---
+++ LearningDiary/Day-7/car.py
@@ -0,0 +1,48 @@
+import json
+
+class MyCar:
+  total = 0
+  __color = ''
+
+  def __init__(self, color):
+    MyCar.total = MyCar.total + 1
+    self.__color = color
+
+  def __del__(self):
+    MyCar.total = MyCar.total - 1
+  
+  def show(self):
+    print(self.__color)
+
+  def getColor(self):
+    return(self.__color)
+
+  @classmethod
+  def howManyCars(cls):
+    return cls.total
+
+if __name__ == "__main__":
+  red = MyCar("red")
+  green = MyCar("green")
+  yellow = MyCar("yellow")
+
+  print(MyCar.total)
+  print(red.getColor())
+  print(green.getColor())
+
+  print(MyCar.howManyCars())
+
+  red.show()
+
+  del red
+
+  print(MyCar.howManyCars())
+
+  # MyCar.show()
+
+  # print(green.__dict__)
+  # json.dump(red, "red.car")
+  green_file = open("green.car", "w")
+  yellow_file = open("yellow.car", "w")
+  json.dump(green.__dict__, green_file)
+  json.dump(yellow.__dict__, yellow_file)
---
+++ LearningDiary/Day-7/main.py
@@ -0,0 +1,9 @@
+import car
+
+red = car.MyCar("red")
+green = car.MyCar("green")
+yellow = car.MyCar("yellow")
+
+print(car.MyCar.total)
+print(red.getColor())
+print(green.getColor())
